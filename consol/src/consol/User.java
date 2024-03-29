262eedb98c2183331af7a9111a8d41314a9b0642
sonchangwoo <thsckddn97@naver.com>
2024-03-29 09:08:00 +0900

    User Create constructor & getter & setter & Override Commit!

--- consol/src/consol/User.java
+++ consol/src/consol/User.java
@@ -2,6 +2,30 @@ package consol;
 
 public class User {
 	
+	private String name,id,password;
+	
+	public User(String name,String id,String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s) [%s/%s]",name,id,password);
+	}
 	
 	
 
