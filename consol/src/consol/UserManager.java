31d3ce870638bc085f1cd81b01c501e643ade570
sonchangwoo <thsckddn97@naver.com>
2024-03-29 09:30:25 +0900

    UserManager Create Singleton!

--- consol/src/consol/UserManager.java
+++ consol/src/consol/UserManager.java
@@ -1,6 +1,17 @@
 package consol;
-
+import java.util.ArrayList;
 public class UserManager {
+
+	private ArrayList<User> group;
+	
+	public UserManager() {
+		group = new ArrayList<>();
+	}
+	public static UserManager userManager = new UserManager();
+	
+	private static UserManager getUserManager() {
+		return userManager;
+	}
 	
 	// User CRUD 기능
 
